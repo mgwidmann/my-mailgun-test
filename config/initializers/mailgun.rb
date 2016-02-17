e39a37b7b2f4d3efffe3fe553cbb93c1a89be26c
Matt Widmann <mgwidmann@gmail.com>
2016-02-17 15:06:10 -0500

    Try configuring differently.

--- config/initializers/mailgun.rb
+++ config/initializers/mailgun.rb
@@ -1,1 +1,6 @@
-$mailgun = Mailgun(api_key: 'key-c749f6b1f3492c74b039d5c5fd2b3ec9')
+Mailgun.configure do |config|
+  config.api_key = 'key-c749f6b1f3492c74b039d5c5fd2b3ec9'
+  config.domain  = 'appb6fe1da58dfe443aa09720e568195cd4.mailgun.org'
+end
+
+$mailgun = Mailgun()
