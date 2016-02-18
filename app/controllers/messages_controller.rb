edcc0e23b161402c32c36b3140a703e974248028
Matt Widmann <mgwidmann@gmail.com>
2016-02-18 10:29:47 -0500

    Clean up HTML so it looks nice!

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -136,13 +136,7 @@ class MessagesController < ApplicationController
                         <table>
                           <tr>
                             <td>
-                              <a href="http://mandrillapp.com/track/click/30252=
-      814/localhost?p=eyJzIjoiY2lYRmdmLV9fckFKbGNHM2Q2alBCSlQ2V0VZIiwidiI6MSwic=
-      CI6IntcInVcIjozMDI1MjgxNCxcInZcIjoxLFwidXJsXCI6XCJodHRwOlxcXC9cXFwvbG9jYWxo=
-      b3N0OjMwMDBcXFwvdmVuZG9yc1xcXC9ob21lXCIsXCJpZFwiOlwiOTA1NjU3Mzg5ZjdjNGNiZjk=
-      2ZjQ4YzY4ODg3N2VhOGRcIixcInVybF9pZHNcIjpbXCJiYjVmMzdlZjEzNjI1ZGQ3NThlMDNhOW=
-      ZmZDU4ZjU2N2UzYjM3NDdmXCJdfSJ9"><img alt="WeddingWire" class="header-ba=
-      se" style="height: auto; width: 222px" src="https://www.weddingwire.com/assets/logos/weddingwire-logo_2x.png"></a>
+                              <img alt="WeddingWire" class="header-base" style="height: auto; width: 222px" src="https://www.weddingwire.com/assets/logos/weddingwire-logo_2x.png">
                             </td>
                           </tr>
                         </table>
@@ -153,8 +147,7 @@ class MessagesController < ApplicationController
                         #{message.stripped_html}
                       </td>
                     </tr>
-                    <tr class="connect-with" style="background: #dfdfdd; marg=
-      in: 0; padding: 0 0 5px" bgcolor="#dfdfdd">
+                    <tr class="connect-with" style="background: #dfdfdd; margin: 0; padding: 0 0 5px" bgcolor="#dfdfdd">
                       <table class="full-width" style="width: 100%">
                         <tr>
                           <td>
@@ -162,45 +155,17 @@ class MessagesController < ApplicationController
                               <td style="color: #626262">
                                 <strong>Connect with WeddingWire:</strong>
                               </td>
-                              <td class="lead-email-sm-padding" style="paddin=
-      g-left: 5px; padding-right: 5px">
-                                <a target="_blank" href="http://mandrillapp.c=
-      om/track/click/30252814/www.facebook.com?p=eyJzIjoidnl3UUF5YmR1cVFESGJuen=
-      BxZUVvVDVpcHdNIiwidiI6MSwicCI6IntcInVcIjozMDI1MjgxNCxcInZcIjoxLFwidXJsXCI6X=
-      CJodHRwOlxcXC9cXFwvd3LmZhY2Vib29rLmNvbVxcXC9XZWRkaW5nV2lyZUVEVVwiLFwiaWRc=
-      IjpcIjkwNTY1NzM4OWY3YzRjYmY5NmY0OGM2ODg4NzdlYThkXCIsXCJ1cmxfaWRzXCI6W1wiMTJ=
-      lNzczNmUxYWIyYzg0Yjg5OGRkYTAxMDYwNjJlNDJlYzBiNTk1NFwiXX0ifQ"><img alt="Fa=
-      cebook" src="http://localhost:3000/assets/vendor/email/fb.gif"></a>
+                              <td class="lead-email-sm-padding" style="padding-left: 5px; padding-right: 5px">
+                                <img alt="Facebook" src="https://www.weddingwire.com/assets/vendor/email/fb.gif">
                               </td>
-                              <td class="lead-email-sm-padding" style="paddin=
-      g-left: 5px; padding-right: 5px">
-                                <a target="_blank" href="http://mandrillapp.c=
-      om/track/click/30252814/twitter.com?p=eyJzIjoibkRHa0NLY01Cc2djQ0VLMVBiUXB=
-      KemlGcDYwIiwidiI6MSwicCI6IntcInVcIjozMDI1MjgxNCxcInZcIjoxLFwidXJsXCI6XCJodH=
-      RwczpcXFwvXFxcL3R3aXR0ZXIuY29tXFxcLlZGRpbmd3aXJlRURVXCIsXCJpZFwiOlwiOTA1N=
-      jU3Mzg5ZjdjNGNiZjk2ZjQ4YzY4ODg3N2VhOGRcIixcInVybF9pZHNcIjpbXCJmODUxMzM3NzFk=
-      MDIwMTFmYTZlM2JmNGNjMDlhYWQ3NTZjOTYxOWIxXCJdfSJ9"><img alt="Twitter" src=
-      ="http://localhost:3000/assets/vendor/email/tw.gif"></a>
+                              <td class="lead-email-sm-padding" style="padding-left: 5px; padding-right: 5px">
+                                <img alt="Twitter" src="https://www.weddingwire.com/assets/vendor/email/tw.gif">
                               </td>
-                              <td class="lead-email-sm-padding" style="paddin=
-      g-left: 5px; padding-right: 5px">
-                                <a target="_blank" href="http://mandrillapp.c=
-      om/track/click/30252814/pinterest.com?p=eyJzIjoiZ1hFOVIzZ0lZblZrOFA2RHZVc=
-      XB6RXNFSmhrIiwidiI6MSwicCI6IntcInVcIjozMDI1MjgxNCxcInZcIjoxLFwidXJsXCI6XCJo=
-      dHRwOlxcXC9cXFwvcGludGVyZXN0LmNvbVxcXC93ZWRkaW5nd2lyZWVkdVxcXC9cIixcImlkXCI=
-      6XCI5MDU2NTczODlmN2M0Y2JmOTZmNDhjNjg4ODc3ZWE4ZFwiLFwidXJsX2lkc1wiOltcImYxNW=
-      M2NmQwYjAzMWVhYTU5Yzg3ODAxMGJiNzA3ZDUxZDU2NDRhOWRcIl19In0"><img alt="Pint=
-      erest" src="http://localhost:3000/assets/vendor/email/pint.gif"></a>
+                              <td class="lead-email-sm-padding" style="padding-left: 5px; padding-right: 5px">
+                                <img alt="Pinterest" src="https://www.weddingwire.com/assets/vendor/email/pint.gif">
                               </td>
-                              <td class="lead-email-sm-padding" style="paddin=
-      g-left: 5px; padding-right: 5px">
-                                <a target="_blank" href="http://mandrillapp.c=
-      om/track/click/30252814/problog.weddingwire.com?p=eyJzIjoib21UcS1TTXkxcUd=
-      6d1ZHNmRxQmFHUDdLWVlNIiwidiI6MSwicCI6IntcInVcIjozMDI1MjgxNCxcInZcIjoxLFwidX=
-      JsXCI6XCJodHRwOlxcXC9cXFwvcHJvYmxvZy53ZWRkaW5nd2lyZS5jb21cXFwvXCIsXCJpZFwiO=
-      lwiOTA1NjU3Mzg5ZjdjNGNiZjk2ZjQ4YzY4ODg3N2VhOGRcIixcInVybF9pZHNcIjpbXCIxYzg2=
-      ZjY4ZGU0NDM4NjRiMjY1MWU0ZDIxMDc1NTRjNWI1YTA3ZjhjXCJdfSJ9"><img alt="Blog"=
-       src="http://localhost:3000/assets/vendor/email/rss.gif"></a>
+                              <td class="lead-email-sm-padding" style="padding-left: 5px; padding-right: 5px">
+                                <img alt="Blog" src="https://www.weddingwire.com/assets/vendor/email/rss.gif">
                               </td>
                             </table>
                           </td>
@@ -211,51 +176,24 @@ class MessagesController < ApplicationController
                       <table class="full-width" style="width: 100%">
                         <tr>
                           <td>
-                            <div class="gray-text" style="color: #5e5e5e">=C2=
-      =A9 WeddingWire, Inc., 2015</div>
+                            <div class="gray-text" style="color: #5e5e5e">WeddingWire, Inc., 2015</div>
                           </td>
                         </tr>
                         <tr></tr>
                         <tr>
                           <td>
-                            <a style="color: #5e5e5e; text-decoration: none" cl=
-      ass="gray-text" href="http://mandrillapp.com/track/click/30252814/local=
-      host?p=eyJzIjoiY2lYRmdmLV9fckFKbGNHM2Q2alBCSlQ2V0VZIiwidiI6MSwicCI6IntcIn=
-      VcIjozMDI1MjgxNCxcInZcIjoxLFwidXJsXCI6XCJodHRwOlxcXC9cXFwvbG9jYWxob3N0OjMwM=
-      DBcXFwvdmVuZG9yc1xcXC9ob21lXCIsXCJpZFwiOlwiOTA1NjU3Mzg5ZjdjNGNiZjk2ZjQ4YzY4=
-      ODg3N2VhOGRcIixcInVybF9pZHNcIjpbXCJiYjVmMzdlZjEzNjI1ZGQ3NThlMDNhOWZmZDU4ZjU=
-      2N2UzYjM3NDdmXCJdfSJ9">Two Wisconsin Circle, 3rd Floor, Chevy Chase, MD 208=
-      15</a>
+                            Two Wisconsin Circle, 3rd Floor, Chevy Chase, MD 208 15
                           </td>
                         </tr>
                         <tr>
                           <td>
-                            <a target="_blank" class="gray-text" href="http=
-      ://mandrillapp.com/track/click/30252814/localhost?p=eyJzIjoiaV9mU1dNb0dEV=
-      VBNNXEwZ0ZkSUtCNm1xYjNnIiwidiI6MSwicCI6IntcInVcIjozMDI1MjgxNCxcInZcIjoxLFwi=
-      dXJsXCI6XCJodHRwOlxcXC9cXFwvbG9jYWxob3N0OjMwMDBcXFwvdmVuZG9yc1xcXC9sb2dpblw=
-      iLFwiaWRcIjpcIjkwNTY1NzM4OWY3YzRjYmY5NmY0OGM2ODg4NzdlYThkXCIsXCJ1cmxfaWRzXC=
-      I6W1wiMjUwMjJiZmZjNDFmMGVlNzQ5YTJlODM4NGVkZWQ1OTNjZDkzZDQ5MVwiXX0ifQ" style=
-      ="color: #5e5e5e">Log In</a>
+                            Log In
                             |
-                            <a target="_blank" class="gray-text" href="mail=
-      to:pros@weddingwire.com" style="color: #5e5e5e">Contact Us</a>
+                            Contact Us
                             |
-                            <a target="_blank" class="gray-text" href="http=
-      ://mandrillapp.com/track/click/30252814/localhost?p=eyJzIjoiX1ZfYklsbkdCU=
-      mRrbjJNY2NhR0JKVGQ2ZUFvIiwidiI6MSwicCI6IntcInVcIjozMDI1MjgxNCxcInZcIjoxLFwi=
-      dXJsXCI6XCJodHRwOlxcXC9cXFwvbG9jYWxob3N0OjMwMDBcXFwvY29ycFxcXC9sZWdhbFxcXC9=
-      wcml2YWN5LXBvbGljeVwiLFwiaWRcIjpcIjkwNTY1NzM4OWY3YzRjYmY5NmY0OGM2ODg4NzdlYT=
-      hkXCIsXCJ1cmxfaWRzXCI6W1wiZDg2M2QwYTU1ZWI1NTFkZTQwODAxZmQzNjNiYjU0M2IyZGZkN=
-      zljNFwiXX0ifQ" style="color: #5e5e5e">Privacy Policy</a>
+                            Privacy Policy
                             |
-                            <a target="_blank" class="gray-text" href="http=
-      ://mandrillapp.com/track/click/30252814/localhost?p=eyJzIjoiM09pd24xZlluR=
-      VBUcXlZQUZpTktiVFp3T2J3IiwidiI6MSwicCI6IntcInVcIjozMDI1MjgxNCxcInZcIjoxLFwi=
-      dXJsXCI6XCJodHRwOlxcXC9cXFwvbG9jYWxob3N0OjMwMDBcXFwvdmVuZG9yc1xcXC92ZW5kb3J=
-      fdXNlcnNcIixcImlkXCI6XCI5MDU2NTczODlmN2M0Y2JmOTZmNDhjNjg4ODc3ZWE4ZFwiLFwidX=
-      JsX2lkc1wiOltcIjk2N2QxOTcwZDAxMDVkN2IyNGE5OWQ4MmViZDQ3MTJlZjE1MmQ4MmNcIl19I=
-      n0" style="color: #5e5e5e">Manage Your Preferences</a>
+                            Manage Your Preferences
                           </td>
                         </tr>
                       </table>
@@ -265,8 +203,7 @@ class MessagesController < ApplicationController
               </tr>
             </table>
           </div>
-        <img src="http://mandrillapp.com/track/open.php?u=30252814&id=90565=
-      7389f7c4cbf96f48c688877ea8d" height="1" width="1"></body>
+        </body>
       </html>
       HTML
     end
